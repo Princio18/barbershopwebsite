c04091618c4426fdf3bc22a4acee70cae33b9872
princio <princio@Macbooks-MacBook-Pro.local>
2024-11-17 12:45:57 +0000

    first commit

---
+++ princio.js
@@ -0,0 +1,25 @@
+
+
+let princio = 4;
+let nadjat = 5;
+
+resultat = console.log(princio+nadjat);
+resultat = console.log(4+5);
+
+resultat = princio + nadjat;
+// alert(resultat);
+
+var kiki = "Je suis";
+var gentille = " une fille"
+resultat1 = console.log(kiki+gentille);
+
+resultat1 = console.log("Je suis "+"une fille");
+resultat1 = console.log("Je suis"+" une fille");
+
+// confirm("do you want to be my friend ?");
+
+function direBonjour(){
+    alert("bonjour");
+}
+direBonjour();
+
